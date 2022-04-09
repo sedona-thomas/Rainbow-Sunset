a5251756282481251f0c1e71029610a868ea0838
Sedona Thomas <sedona.thomas@columbia.edu>
2022-04-09 17:41:10 -0400

    Create wifi_handler.h

---
+++ Rainbow Sunset/src/wifi_handler.h
@@ -0,0 +1,24 @@
+/**
+ * The wifi handler controls an ESP32 wifi connection
+ *
+ * @author Sedona Thomas
+ * @version 2.0.0
+ */
+
+#ifndef __WIFI_HANDLER_H__
+#define __WIFI_HANDLER_H__
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "glass.h"
+#include "esp32_screen.h"
+
+#define USE_SERIAL Serial /**< serial communication */
+#define SECOND 1000       /**< miliseconds (1000 miliseconds == 1 second) */
+#define TESTING false     /**< sets testing mode */
+
+void wifiSetup();
+void wifiLoop();
+
+#endif
