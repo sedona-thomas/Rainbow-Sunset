cc2318ff1f0f381e78dcc04a992d4413614e6c2f
Sedona Thomas <sedona.thomas@columbia.edu>
2022-04-09 17:09:29 -0400

    Create glass.cpp

---
+++ Rainbow Sunset/src/glass.cpp
@@ -0,0 +1,33 @@
+#include "glass.h"
+
+/**
+ * Glass constructor makes a stained glass art object
+ */
+Glass::Glass()
+{
+  name = "stained glass";
+}
+
+/**
+ * Glass constructor makes a stained glass art object
+ *
+ * @param name_in the stained glass art name
+ */
+Glass::Glass(std::string _name)
+{
+  name = _name;
+}
+
+/**
+ * The setup method sets up the stained glass art
+ */
+void Glass::setup()
+{
+}
+
+/**
+ * The run method runs the stained glass art
+ */
+void Glass::run()
+{
+}
