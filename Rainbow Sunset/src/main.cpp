59ccca214dd9e9e4386a4c90e9898679bcdaf445
Sedona Thomas <sedona.thomas@columbia.edu>
2022-04-09 17:09:33 -0400

    Update main.cpp

--- Rainbow Sunset/src/main.cpp
+++ Rainbow Sunset/src/main.cpp
@@ -1,13 +1,5 @@
 #include <Arduino.h>
 
-// #include "brush.h"
-// #include "button.h"
-// #include "esp32_screen.h"
-// #include "joystick.h"
-// #include "potentiometer.h"
-// #include "servo.h"
-// #include "step.h"
-
 void setup()
 {
   // put your setup code here, to run once:
