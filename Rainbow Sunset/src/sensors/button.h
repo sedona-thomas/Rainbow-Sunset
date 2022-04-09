3a2b3410fd894d52934a21fead272b6972155c94
Sedona Thomas <sedona.thomas@columbia.edu>
2022-04-09 17:40:34 -0400

    Create button.h

---
+++ Rainbow Sunset/src/sensors/button.h
@@ -0,0 +1,24 @@
+#ifndef __BUTTON_H__
+#define __BUTTON_H__
+
+#include "serial_communication.h"
+#include "sensor.h"
+
+#define BUTTON_DELAY 50 /**< delays button if defined */
+
+/**
+ * The Button class controls the necessary features of a button
+ * 
+ * @author Sedona Thomas
+ * @version 2.0.0
+ */
+class Button : public Sensor, public SerialCommunication {
+public:
+  Button(){};
+  Button(std::string, int, bool);
+  Button(int, bool);
+  void read();
+  void send();
+};
+
+#endif
