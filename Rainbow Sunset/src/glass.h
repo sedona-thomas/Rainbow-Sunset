1c9dccb92aa4bf1e3b566c168f66f029e9447acc
Sedona Thomas <sedona.thomas@columbia.edu>
2022-04-09 17:09:31 -0400

    Create glass.h

---
+++ Rainbow Sunset/src/glass.h
@@ -0,0 +1,34 @@
+#ifndef __GLASS_H__
+#define __GLASS_H__
+
+#include <Arduino.h>
+#include <string>
+#include "esp32_screen.h"
+// #include "brush.h"
+// #include "button.h"
+// #include "joystick.h"
+// #include "potentiometer.h"
+// #include "servo.h"
+// #include "step.h"
+
+#define SECOND 1000 /**< miliseconds (1000 miliseconds == 1 second) */
+
+/**
+ * The Glass class controls the stained glass sunset
+ *
+ * @author Sedona Thomas
+ * @version 1.0.0
+ */
+class Glass
+{
+public:
+  Glass();
+  Glass(std::string);
+  void setup();
+  void run();
+
+private:
+  std::string name;
+};
+
+#endif
