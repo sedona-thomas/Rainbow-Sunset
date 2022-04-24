ff484ee94ca0ed69fbfd8614899682f57c419331
Sedona Thomas <sedona.thomas@columbia.edu>
2022-04-24 18:36:45 -0400

    Create no_wifi.h

---
+++ Rainbow Sunset/src/no_wifi.h
@@ -0,0 +1,25 @@
+/**
+ * The wifi handler controls an ESP32 wifi connection
+ *
+ * @author Sedona Thomas
+ * @version 2.0.0
+ */
+
+#ifndef __NO_WIFI_H__
+#define __NO_WIFI_H__
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "glass.h"
+#include "esp32_screen.h"
+
+#define USE_SERIAL Serial /**< serial communication */
+#define SECOND 1000       /**< miliseconds (1000 miliseconds == 1 second) */
+#define TESTING false     /**< sets testing mode */
+#define BAUD_RATE 115200  /**< baud rate of wifi communication */
+
+void noWifiSetup();
+void noWifiLoop();
+
+#endif
